97f1d2740b25bf00423f3bd05f726e7d55a14a09
takuho24n <long_lagoon24takuho@hotmail.co.jp>
2020-10-13 17:08:22 +0900

    First Commit_relationship

--- config/routes.rb
+++ config/routes.rb
@@ -4,14 +4,23 @@ Rails.application.routes.draw do
   # get 'sessions/destroy'
   # get 'users/new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  
+  resources :users do
+
+  #follow and unfollow
+  resources :relationships, only: [:create, :destroy]
+
   root 'pages#home'
 
   get '/about', to: 'pages#about'
   get '/contact', to: 'pages#contact'
   get '/signup', to: 'users#new'
 
-  resources :users
+   
+  resources :users do
+    member do
+    get :following, :followers
+    end
+  end
 
   
 end
