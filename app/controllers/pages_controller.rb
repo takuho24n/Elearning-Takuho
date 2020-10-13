45f55047fe83c7ef8f7291d74c857e5fe6562f89
takuho24n <70140553+takuho24n@users.noreply.github.com>
2020-10-13 18:05:56 +0900

    Login (#2)
    * Inital Commit _signup
    
    * Inital Commit_login
    
    * Third Commit_login
    
    * Fixed Gemfile.lock
    
    * Fixed login
    
    * Fixed_2_login

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,10 @@
 class PagesController < ApplicationController
+  
   def home
-        
+    
+    if logged_in?
+      @user = current_user
+    end      
+  
   end 
 end
---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,25 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      flash[:success] = "Save Successfully!!"
+      log_in user
+      redirect_to user
+    else
+      render 'new'
+      flash.now[:danger] = "Invalid content. Try again."
+    end
+
+  end
+
+  def destroy
+
+    log_out
+    redirect_to root_url
+
+  end
+end
