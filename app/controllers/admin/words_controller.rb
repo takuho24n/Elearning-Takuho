8db17b475dbca93902ab05fbef96930dff25a2ec
takuho24n <long_lagoon24takuho@hotmail.co.jp>
2020-11-03 17:21:39 +0900

    Second Commit_word

--- app/controllers/admin/words_controller.rb
+++ app/controllers/admin/words_controller.rb
@@ -1,5 +1,5 @@
 class Admin::WordsController < ApplicationController
-    before_action :only_loggedin_users, only: [:show, :index]
+    before_action :only_loggedin_users
     before_action :admin_user
     before_action :require_admin
 
@@ -13,30 +13,42 @@ class Admin::WordsController < ApplicationController
 
     def new
       @category = Category.find(params[:category_id])
-      @words = @category.words.all
+      #@words = @category.words.build
+      @words = @category.words.new
       #@word = Word.build.find(params[:word_id])
     end
 
     def edit
       @category = Category.find(params[:category_id])
       @word = @category.words.find(params[:id])
-      @word = @category.words.build
+      #@word = @category.words.build
+    end
+
+    def update
+      @category = Category.find(params[:category_id])
+      @word = @category.words.find(params[:id])
+  
+      if @word.update_attributes(word_params)
+        redirect_to admin_category_url(@category)
+      else
+        redirect_to admin_category_word_url(@category)
+      end
     end
 
     def create
       @category = Category.find_by_id(params[:category_id])
-      @word = @category.words.build(word_params)
+      @word = @category.words.new(word_params)
         if @word.save
-          redirect_to admin_categories_url
+          redirect_to new_admin_category_word_url
         else
-          @feed_items = nil
+          #@feed_items = nil
           render 'new'
         end 
     end 
   
     def destroy
       Word.find(params[:id]).destroy
-      redirect_to admin_categories_url
+      redirect_to admin_category_words_url
     end
   
     private
